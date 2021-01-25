6d9e9b8b3b3df513262bea1070fd8aad8a8c2efe
sahamnadeem <sahamnadeem2001@outlook.com>
2021-01-26 00:25:32 +0500

    working socket

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('request:online-users',(data)=>{
+    console.log('api hit'+data.token);
+    socket.emit('response:online-users',[{user_id:"_2432444243"},{user_id:"_2432444243"},{user_id:"_2432444243"},{user_id:"_2432444243"},{user_id:"_2432444243"},{user_id:"_2432444243"},{user_id:"_2432444243"}])
+  })
+});
+
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
